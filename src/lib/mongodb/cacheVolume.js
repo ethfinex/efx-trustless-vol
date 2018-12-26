c4b3e8c771626add2052eabebe23a15ae723e037
hems <hems.inlet@gmail.com>
2018-12-26 19:51:33 +0000

    adding date limits

--- src/lib/mongodb/cacheVolume.js
+++ src/lib/mongodb/cacheVolume.js
@@ -2,7 +2,14 @@ const moment = require('moment')
 const getDailyVolume = require('../getDailyVolume')
 const cache = require('./cache')
 
+// 13th of September 2018
+const firstDay = moment.utc().year(2018).month(8).date(13)
+
 module.exports = (day, month, year) => {
+
+  // current date
+  const today = moment.utc().startOf('day')
+
   const date = moment.utc()
     .year(year)
     .month(month)
@@ -12,6 +19,14 @@ module.exports = (day, month, year) => {
     .seconds(0)
 
   const timestamp = date.unix()
+
+  if(timestamp < firstDay.unix()){
+    return { error: 'date_is_too_early' }
+  }
+
+  if(timestamp >= today.unix()){
+    return { error: 'day_must_be_complete' }
+  }
 
   const query = {timestamp}
 
