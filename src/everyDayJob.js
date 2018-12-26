abefc1abeec906d8d9104535b5ad3620bb1529bd
hems <hems.inlet@gmail.com>
2018-12-26 19:55:42 +0000

    add instruction to automatically cache volume at the beginning of the day

--- src/everyDayJob.js
+++ src/everyDayJob.js
@@ -3,15 +3,19 @@ const cron = require('node-cron')
 
 const moment = require('moment')
 
+const cacheVolume = require('./lib/mongodb/cacheVolume')
+
 module.exports = async () => {
   // every day
   cron.schedule('0 0 * * *', async () => {
     console.log( "Daily job running")
     console.log( " ---> date: ", moment.utc().format() )
 
-    // start caching volume for previous days
+    // cache result for previous day
     const day = moment().utc().date()
     const month = moment().utc().month()
     const year = moment().utc().year()
+
+    return cacheVolume(day -1, month, year)
   })
 }
