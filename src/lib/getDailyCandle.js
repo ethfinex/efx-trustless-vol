c639eb000b0b9da84b5aa12f8661a18e05925ca4
hems <hems.inlet@gmail.com>
2019-02-14 11:57:23 +0000

    adding retry logic to getDailyCandle, adding some console.logs to debug the rate limit error

--- src/lib/getDailyCandle.js
+++ src/lib/getDailyCandle.js
@@ -1,8 +1,32 @@
+const sleep = require('sleep-promise')
 const {get} = require('request-promise')
 
-module.exports = (symbol, dayTimestamp) => {
+module.exports = async (symbol, dayTimestamp) => {
   let url = 'https://api.bitfinex.com/v2/candles/trade:1D:' + symbol
   url = url + '/hist?limit=1&end=' + dayTimestamp
 
-  return get(url, {json: true})
+  let retries = 0
+
+  while(true) {
+
+    try {
+      return await get(url, {json: true})
+    } catch(e) {
+
+      console.log("")
+      console.log("error getting daily candle for: ", symbol)
+      console.log(e)
+
+      if(retries > 2){
+        throw(e)
+      }
+    }
+
+    console.log("retrying in 500ms")
+    console.log("")
+
+    retries += 1
+    await sleep(500)
+  }
+
 }
